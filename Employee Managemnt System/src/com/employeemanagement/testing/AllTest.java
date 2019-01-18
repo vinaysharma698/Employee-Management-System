f715c62c65ca6403e63e5e405a848f5da6fa61c3
vinaysharma698 <vinay.sharma@psquickit.com>
2019-01-18 15:21:52 +0530

    updated test cases

--- Employee Managemnt System/src/com/employeemanagement/testing/AllTest.java
+++ Employee Managemnt System/src/com/employeemanagement/testing/AllTest.java
@@ -14,6 +14,8 @@ class AllTest {
 	@Test
 	void testAddEmployee() {
 		Pojo input = new Pojo();
+		
+		//Test to create Employee
 		input.setEmployeeId(109);
 		input.setEmployeeName("Vinay kumar Sharma");
 		input.setAddress("Gzb");
@@ -21,20 +23,17 @@ class AllTest {
 		int create =e.addEmployee(input);
 		assertEquals(1,create);
 		
-	}
-	@Test
-	void test1AddEmployee() {
-		Pojo input = new Pojo();
-		input.setEmployeeId(109);
+		//here the phone no exceeds 10 digits
+		//So it will fail
+		
+		/*input.setEmployeeId(109);
 		input.setEmployeeName("Vinay kumar Sharma");
 		input.setAddress("Gzb");
 		input.setPhoneNo("12345678900000");
 		int create1 =e.addEmployee(input);
-		assertEquals(1,create1);
-	}
-	@Test
-	void testViewEmployee() {
-		Pojo input = new Pojo();
+		assertEquals(1,create1);*/
+		
+		//Test to view employee
 		input.setEmployeeId(109);
 		ArrayList<Pojo> expectedOutput =new ArrayList<Pojo>();
 		EmployeeDAOImpl e= new EmployeeDAOImpl();
@@ -46,82 +45,51 @@ class AllTest {
 		expectedOutput.add(expOutput);
 		assertEquals(expectedOutput.get(0).getEmployeeId(),
 				e.viewEmployee(input).get(0).getEmployeeId());
-
-	}
-	@Test
-	void testUpdateEmployee() {
-		Pojo input = new Pojo();
+		
+		//Test to update employee
 		input.setEmployeeId(109);
 		input.setEmployeeName("vinay");
 		input.setAddress("Gzb");
 		input.setPhoneNo("1234567890");
 		int update =e.updateEmployee(input);
 		assertEquals(1,update);
-	}
-	
-	/*@Test
-	void testAddProject() {
-		Pojo input = new Pojo();
-		input.setProjectId(101);
-		input.setProjectDesciption("This is a demo project");
-		int addProject = e.addProject(input);
-		assertEquals(1,addProject);
 		
-	}*/
-	@Test
-	void testUpdateProject() {
-		Pojo input = new Pojo();
+		//Test to update Project
 		input.setProjectId(101);
 		input.setProjectDesciption("updated");
 		int updateProject = e.updateProject(input);
 		assertEquals(1,updateProject);
-		
-	}
-	/*@Test
-	void testAssignSalary() {
-		Pojo input = new Pojo();
-		input.setEmployeeId(109);
-		input.setSalary("1000");
-		int salaryAssigned = e.assignSalary(input);
-		assertEquals(1,salaryAssigned);
-		
-	}
-	@Test
-	void testUpdateSalary() {
-		Pojo input = new Pojo();
-		input.setEmployeeId(109);
-		input.setSalary("10000");
-		int salaryUpdated= e.updateSalary(input);
-		assertEquals(1,salaryUpdated);
-		
-	}*/
 	
-	@Test
-	void testAssignProject() {
-		Pojo input = new Pojo();
+		//Test to Assign Project
 		input.setEmployeeId(109);
 		input.setProjectId(101);
 		int assignProject= e.assignProject(input);
 		assertEquals(1,assignProject);
 		
-	}
-	void testUnassignProject() {
-		Pojo input = new Pojo();
+		//Test to Unassign Project
 		input.setEmployeeId(109);
 		input.setProjectId(101);
 		int unassignProject= e.unassignProject(input);
 		assertEquals(1,unassignProject);
 		
-	}
-
-	
-	
-	
-	@Test
-	void testDeleteEmployee() {
-		Pojo input = new Pojo();
+		//Test to assign Salary
+		input.setEmployeeId(109);
+		input.setSalary("1000");
+		System.out.println(input.getSalary());
+		System.out.println(input.getEmployeeId());
+		int salaryAssigned = e.assignSalary(input);
+		assertEquals(1,salaryAssigned);
+		
+		//Test to update Salary
+		input.setEmployeeId(109);
+		input.setSalary("10000");
+		int salaryUpdated= e.updateSalary(input);
+		assertEquals(1,salaryUpdated);
+		
+		//Test to delete Employee
 		input.setEmployeeId(109);
 		int delete =e.deleteEmployee(input);
 		assertEquals(1,delete);
 	}
+	
 }
